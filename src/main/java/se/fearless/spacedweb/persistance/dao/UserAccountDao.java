ab3739255d42a6ef6dfc65e2cfd0e01f558a8974
Magnus Gustafsson <magnus.gustafsson@king.com>
2015-04-22 09:29:08 +0200

    Update application properties

--- src/main/java/se/fearless/spacedweb/persistance/dao/UserAccountDao.java
+++ src/main/java/se/fearless/spacedweb/persistance/dao/UserAccountDao.java
@@ -1,9 +1,11 @@
 package se.fearless.spacedweb.persistance.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.fearless.common.uuid.UUID;
 import se.fearless.spacedweb.model.UserAccount;
 
+@Repository
 public interface UserAccountDao extends JpaRepository<UserAccount, UUID> {
 	public UserAccount findByUsername(String username);
 
